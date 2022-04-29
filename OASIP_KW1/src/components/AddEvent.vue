7af0f22ad55717ab691302f409b06f3695c88caa
Wareewan Pongpunchaikul <68785361+wareewan-opal@users.noreply.github.com>
2022-04-30 02:20:34 +0700

    add select + date/time

--- OASIP_KW1/src/components/AddEvent.vue
+++ OASIP_KW1/src/components/AddEvent.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref,onBeforeMount } from 'vue'
+
 </script>
  
 <template>
     <p class="addtitle">Add Events / Booking</p>
-    <p class="select">Select Category do you need</p>
+    <div class="select1">
+        <p class="select">Select Category do you need</p>
 <select class="option">
     <option value="1">Project Management Clinic</option>
     <option value="2">DevOps/Infra Clinic</option>
@@ -12,10 +13,17 @@ import { ref,onBeforeMount } from 'vue'
     <option value="4">Client-side Clinic</option>
     <option value="5">Server-side Clinic</option>
 </select>
+    </div>
+<div class="select2">
+    <p>Date / Time</p>
+    <input id="party" type="datetime-local" name="partydate">
+</div>
+
 </template>
  
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Changa+One&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
 .addtitle{
  font-family: 'Changa One', cursive;
  font-size: 3em;
@@ -25,8 +33,27 @@ import { ref,onBeforeMount } from 'vue'
 .select{
     font-size: larger;
     margin-left: 2em;
+    font-family: 'Changa One', cursive;
+     text-align: center;
 }
 .option{
     margin-left: 2.5em;
+    text-align: center;
+}
+.select2{
+    font-size: larger;
+    margin-left: 2em;
+    font-family: 'Changa One', cursive;
+     text-align: center;
+}
+.select1{
+    /* font-size: larger; */
+    margin-left: 2em;
+    /* font-family: 'Changa One', cursive; */
+    text-align: center; 
+}
+.party{
+    font-family: 'Itim', cursive;
+    margin-left: 2em
 }
 </style>
