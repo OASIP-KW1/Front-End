e56539114756193b2e472ac5c7bb5d09c5e2eaed
Wareewan Pongpunchaikul <68785361+wareewan-opal@users.noreply.github.com>
2022-05-01 00:08:37 +0700

    edit category + add picture

--- OASIP_KW1/src/components/Category.vue
+++ OASIP_KW1/src/components/Category.vue
@@ -8,19 +8,26 @@ defineProps({
 </script>
  
 <template>
-    <p class="titleCategory">Category</p>
-<div class="modal-mask">
-      <div class="modal-wrapper">
-      <div v-for="(eventCategory,index) in eventCategory">
-        <div class="modal-container">
-        <p>ID : {{eventCategory.id}}</p>
-        <p>Clinic Name : {{eventCategory.eventCategoryName}}</p>
-        <p>Description : {{eventCategory.eventCategoryDescription}}</p>
-        <p>Duration : {{eventCategory.eventDuration}} minute</p>
+<p class="titleCategory">Category</p>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div v-for="(eventCategory,index) in eventCategory">
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">ID : {{eventCategory.id}}</h5>
+        <p class="card-text">Clinic Name : {{eventCategory.eventCategoryName}}</p>
+        <img src="../assets/project-management.png" class="card-img-top" alt="project-management" v-if="eventCategory.id == 1">
+        <img src="../assets/dev-ops.png" class="card-img-top" alt="dev-ops" v-if="eventCategory.id == 2">
+        <img src="../assets/database.png" class="card-img-top" alt="database" height="325" v-if="eventCategory.id == 3">
+        <img src="../assets/client-side.png" class="card-img-top" alt="client-side" width="80%" height="350" v-if="eventCategory.id == 4">
+        <img src="../assets/back-end.png" class="card-img-top" alt="back-end" v-if="eventCategory.id == 5">
+        <p class="card-text">Description : {{eventCategory.eventCategoryDescription}}</p>
+        <p class="card-text">Duration : {{eventCategory.eventDuration}} minute</p>
       </div>
-        </div>
-      </div>
-</div>
+    </div>
+  </div>
+  </div>
+  </div>
 </template>
  
 <style>
