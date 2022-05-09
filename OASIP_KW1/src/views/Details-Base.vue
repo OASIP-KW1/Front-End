14305bdc66754a824d3ccdac947832fe7a4e162b
Wareewan Pongpunchaikul <68785361+wareewan-opal@users.noreply.github.com>
2022-05-09 23:06:43 +0700

    edit path fetch

--- OASIP_KW1/src/views/Details-Base.vue
+++ OASIP_KW1/src/views/Details-Base.vue
@@ -8,7 +8,7 @@ const {params} = useRoute()
 let eventDetail = ref({})
 // GET
 const getEvents = async () =>{
-    const res = await fetch(`http://intproj21.sit.kmutt.ac.th/kw1/events/${params.id}`)
+    const res = await fetch(`${import.meta.env.BASE_URL}api/events/${params.id}`)
     if(res.status === 200) {
     console.log(`response.status: ${res.status}`)
     console.log(res);
@@ -32,7 +32,7 @@ const toEditingNode = async (editNote) => {
 // })
 
 const modifyEvent = async (events) => {
-  const res = await fetch(`http://intproj21.sit.kmutt.ac.th/kw1/events/${params.id}`, {
+  const res = await fetch(`${import.meta.env.BASE_URL}api/events/${params.id}`, {
     method: 'PUT',
     headers: {
       'content-type': 'application/json'
